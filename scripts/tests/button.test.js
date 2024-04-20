91cfa8985a708f1fdd73a9330377f4ea230db205
CedricNtwari <cedric.ntwari@powercoders.org>
2024-04-21 01:28:36 +0200

    Mock entire html page

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -5,7 +5,11 @@
 const buttonClick = require('../buttton')
 
 beforeEach(() => {
-  document.body.innerHTML = '<p id="par"></p>'
+  const fs = require('fs')
+  const fileContents = fs.readFileSync('index.html', 'utf-8')
+  document.open()
+  document.write(fileContents)
+  document.close()
 })
 
 describe('DOM tests', () => {
@@ -13,6 +17,9 @@ describe('DOM tests', () => {
     it('should change the text', () => {
       buttonClick()
       expect(document.getElementById('par').innerHTML).toBe('You Clicked')
+    })
+    it('h1 should exist', () => {
+      expect(document.getElementsByTagName('h1').length).toBe(1)
     })
   })
 })
